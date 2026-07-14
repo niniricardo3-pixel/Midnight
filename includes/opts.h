e575ca972042cb6c8820995037040e3649a4c651
niniricardo3-pixel <niniricardo3@gmail.com>
2026-07-14 10:19:19 +0200

    Change BACKEND_URL to local IP address

--- includes/opts.h
+++ includes/opts.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr const char *BACKEND_URL = "http://lwxi5lovhq.localto.net:3999";
+constexpr const char *BACKEND_URL = "http://127.0.0.1:3551";
 static const char* EPIC_DOMAINS[] = {
     "game-social.epicgames.com",
     "ol.epicgames.com",
