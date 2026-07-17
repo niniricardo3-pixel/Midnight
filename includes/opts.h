b4b8c0a897e5e87c9cd4635e5c326252b7e4d31a
niniricardo3-pixel <niniricardo3@gmail.com>
2026-07-17 20:49:15 +0200

    Update opts.h

--- includes/opts.h
+++ includes/opts.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr const char *BACKEND_URL = "http://178.211.228.188:43624";
+constexpr const char *BACKEND_URL = "http://178.211.228.188";
 static const char* EPIC_DOMAINS[] = {
     "game-social.epicgames.com",
     "ol.epicgames.com",
