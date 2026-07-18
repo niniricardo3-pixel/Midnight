4cb34f921a86dbb00977e561f79f30f3e32fe3b2
niniricardo3-pixel <niniricardo3@gmail.com>
2026-07-18 07:44:51 +0200

    Update BACKEND_URL to new IP address

--- includes/opts.h
+++ includes/opts.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr const char *BACKEND_URL = "http://178.211.228.188";
+constexpr const char *BACKEND_URL = "http://172.22.22.122";
 static const char* EPIC_DOMAINS[] = {
     "game-social.epicgames.com",
     "ol.epicgames.com",
